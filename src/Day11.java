f41a87d415427b957641456527b94de3e98cddeb
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2023-12-12 00:48:57 +0100

    AOC-2023: Day 11 - part 2 - expand multiplier

--- src/Day11.java
+++ src/Day11.java
@@ -10,28 +10,39 @@ public class Day11 {
     Space sample = expand(InputReader.readInput("resources/day11test.txt"));
     Space real = expand(InputReader.readInput("resources/day11.txt"));
     System.out.println("____________");
-    System.out.println("part one test: " + day11.partOne(sample));
-    System.out.println("part one real: " + day11.partOne(real));
+    System.out.println("part one test: " + day11.partOne(sample, 1L));
+    System.out.println("part one real: " + day11.partOne(real, 1L));
     System.out.println("____________");
-    System.out.println("part two test: " + day11.partTwo(sample));
-    System.out.println("part two real: " + day11.partTwo(real));
+    System.out.println("part two test 10: " + day11.partOne(sample, 10L));
+    System.out.println("part two test 100: " + day11.partOne(sample, 100));
+    System.out.println("part two test 1M: " + day11.partOne(sample, 1_000_000L));
+    System.out.println("part two real 1M: " + day11.partOne(real, 1_000_000L));
     System.out.println("____________");
   }
 
-  private int partOne(Space grid) {
+  private long partOne(Space grid, long multiplier) {
     Map<Integer, Galaxy> galaxies = grid.galaxies;
     List<Integer> keys = galaxies.keySet().stream().toList();
-    Map<Set<Integer>, Integer> distances = new HashMap<>();
+    Map<Set<Integer>, Long> distances = new HashMap<>();
     for (int i = 0; i < keys.size(); i++) {
       for (int j = i + 1; j < keys.size(); j++) {
         Galaxy left = galaxies.get(keys.get(i));
         Galaxy right = galaxies.get(keys.get(j));
-        int distance = Math.abs(left.c - right.c) + Math.abs(left.r - right.r);
-        distances.put(Set.of(left.index, right.index), distance);
+        var emptyRowsBetween =
+            grid.emptyRows.stream()
+                .filter(row -> row > Math.min(left.r, right.r) && row < Math.max(left.r, right.r))
+                .count();
+        var emptyColsBetween =
+            grid.emptyColumns.stream()
+                .filter(col -> col > Math.min(left.c, right.c) && col < Math.max(left.c, right.c))
+                .count();
+        long empty = Math.abs(emptyColsBetween * multiplier) + (emptyRowsBetween * multiplier) - emptyColsBetween - emptyRowsBetween;
+        long distance = Math.abs(left.c-right.c) + Math.abs(left.r-right.r) + empty;
+        distances.put(Set.of(left.index, right.index),distance);
       }
     }
     // visualize(grid, distances);
-    return distances.values().stream().reduce(Integer::sum).orElseThrow();
+    return distances.values().stream().reduce(Long::sum).orElseThrow();
   }
 
   private void visualize(Space grid, Map<Set<Integer>, Integer> distances) {
@@ -52,53 +63,35 @@ public class Day11 {
     System.out.println(distances);
   }
 
-  private int partTwo(Space grid) {
-    return 0;
-  }
-
   private static Space expand(List<String> input) {
-    List<String> expanded = new ArrayList<>();
     // expand rows
     List<Integer> emptyRows = new ArrayList<>();
     for (int i = 0; i < input.size(); i++) {
       String line = input.get(i);
-      expanded.add(line);
       if (!line.contains("#")) {
         emptyRows.add(i);
-        expanded.add(line);
       }
     }
     // expand columns
     List<Integer> emptyColumns = new ArrayList<>();
-    int cols = expanded.getFirst().length();
+    int cols = input.getFirst().length();
     for (int c = 0; c < cols; c++) {
       int count = 0;
-      for (String string : expanded) {
+      for (String string : input) {
         if (string.charAt(c) == '.') count++;
       }
-      if (count == expanded.size()) emptyColumns.add(c);
-    }
-    for (int i = 0; i < expanded.size(); i++) {
-      StringBuilder sb = new StringBuilder();
-      for (int j = 0; j < expanded.get(i).length(); j++) {
-        sb.append(expanded.get(i).charAt(j));
-        if (emptyColumns.contains(j)) {
-          sb.append(".");
-        }
-      }
-      expanded.set(i, sb.toString());
+      if (count == input.size()) emptyColumns.add(c);
     }
     Map<Integer, Galaxy> galaxies = new HashMap<>();
     int galaxy = 1;
-    for (int i = 0; i < expanded.size(); i++) {
-      for (int j = 0; j < expanded.get(i).length(); j++) {
-        if (expanded.get(i).charAt(j) == '#') {
+    for (int i = 0; i < input.size(); i++) {
+      for (int j = 0; j < input.get(i).length(); j++) {
+        if (input.get(i).charAt(j) == '#') {
           galaxies.put(galaxy, new Galaxy(galaxy++, i, j));
         }
       }
     }
-    return new Space(
-        expanded.size(), expanded.getFirst().length(), galaxies, emptyRows, emptyColumns);
+    return new Space(input.size(), input.getFirst().length(), galaxies, emptyRows, emptyColumns);
   }
 
   record Space(
